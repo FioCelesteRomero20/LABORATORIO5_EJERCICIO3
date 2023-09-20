e16c279b40434db35fde1efd03ac8b4b36d6e740
PC <PC@DESKTOP-NOE8GP1>
2023-09-20 13:04:07 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO5_EJERCICIO3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO5_EJERCICIO3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de notas: ");
+            int notas = int.Parse(Console.ReadLine());
+
+            float nota = 0;
+            float promedio = 0f;
+
+            for (int i = 1; i <= notas; i++)
+            {
+                Console.WriteLine("Ingrese la nota " + i + ":");
+                int num = int.Parse(Console.ReadLine());
+
+                promedio = promedio + num;
+            }
+            
+            promedio = promedio / notas;
+            Console.WriteLine("El promedio es: " + promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
